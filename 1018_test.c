5fa153b0de2fc74b35bffb10ca6237bf0e494394
Seanwu931216 <liangyiwu0@gmail.com>
2023-10-19 23:55:06 +0800

    Update 1018_test.c

--- 1018_test.c
+++ 1018_test.c
@@ -1,8 +1,17 @@
-include <stdio.h>
+#include <stdio.h>
+#define board_size = 8
 
-int maim(){
-
-int chess[8][8],chess[i][j];
-scanf("%d",&)
-
+int main() {
+    int i, j,k;
+    int chess_1[8][8],chessy[i][j];
+    
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
+            scanf("%d\n", &(chess_1[i][j]));
+        }
+    }
+    if()
+    
+    
+    return 0;
 }
