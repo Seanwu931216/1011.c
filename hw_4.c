2073493797154ec05bab50fa807af096aebd75fe
Seanwu931216 <147570661+Seanwu931216@users.noreply.github.com>
2024-01-08 00:02:11 +0800

    Update hw_4.c

--- hw_4.c
+++ hw_4.c
@@ -1,34 +1,91 @@
 #include <stdio.h>
-#define board_size  8
+#define BOARD_SIZE 8
 
-int main() {
-    int i, j,value,k;
-    int chessy[8][8];
-    
-    for (i = 0; i < 8; i++) {
-        for (j = 0; j < 8; j++) {
-            scanf("%d", &(chessy[i][j]));//i雖然是x座標，但決定的是哪一列，而j則是決定哪一行
-        }
-    }
-    int x ,y;
-    scanf("%d %d",&x,&y);
-    value = chessy[x][y];
-    if(value != 0){
-        for ( i = -1; i <= 1; i++){//1,0,-1代表方向向量
-            for(j = -1;j<= 1 ;j++){
-                if( i==0 && j==0)continue;//待在自己原本的的位置上，故不考慮，繼續for迴圈
-                for(k=1;k<board_size;k++){
-                    if(x+i*k<0 ||x+i*k>(board_size-1)||//i、j從0~7，需考慮大於7小於0的可能
-                       y+j*k<0 ||y+j*k>(board_size-1)||
-                       chessy[x+i*k][y+j*k]==value)break;//從原本位置開始找，找到跟自己同色則breeak
-                    if(chessy[x+i*k][y+j*k]==0){
-                        if(k!=1){
-                            printf("%d,%d\n",x+i*k,y+j*k);
-                        }
+int find_chess(int arr[BOARD_SIZE][BOARD_SIZE],int x,int y,int Disk){
+    int i,j,k;
+    int found;
+    switch (Disk) {
+    case 0:
+        //方向向量
+        for (i=-1; i<=1; ++i)
+            for (j=-1; j<=1; ++j) {
+                if (i==0 && j==0)
+                    continue;
+                //k代表步數
+                int temp,value;
+                for (k=1; k<=BOARD_SIZE; ++k) {
+                    if (x+i*k<0 || x+i*k>=BOARD_SIZE ||
+                        y+j*k<0 || y+j*k>=BOARD_SIZE ||
+                        arr[x+i*k][y+j*k]==0) 
+                            break;
+                    value = arr[x+i*k][y+j*k];
+                    temp = arr[x+i*(k+1)][y+j*(k+1)];
+                    if (k!=1 && found!=temp){
+                        return 1;
                     }
+                }
+                return 0;
+            }
+        if (!found)
+            return 0;
+        break;
+    case 1:
+        return 0;
+        break;
+    case 2:
+        return 0;
+        break;
+    }
+}
+void print_steps(int arr[BOARD_SIZE][BOARD_SIZE],int x,int y ,int Color){
+    for(int i=-1;i<=1;i++)
+        for(int j=-1;j<=1;j++){
+            //k代表步數
+            int temp = 0,value = 0;
+            for (int k=1; k<=BOARD_SIZE; ++k) {
+                if (x+i*k<0 || x+i*k>=BOARD_SIZE ||
+                    y+j*k<0 || y+j*k>=BOARD_SIZE ||
+                    arr[x+i*k][y+j*k]==Color) 
+                    break;
+                if(k == 1 && arr[x+i*k][y+j*k] == 0)
+                    break;
+                value = arr[x+i*k][y+j*k];
+                temp = arr[x+i*(k-1)][y+j*(k-1)];
+                if (value == 0 && temp != Color && temp != 0) {
+                        printf("(%d,%d)",x+i*k,y+j*k);
                 }
             }
         }
+}
+
+int main() {
+    int  posX, posY,sltDisk;
+    int reversi[BOARD_SIZE][BOARD_SIZE] = {
+                     {0,0,0,0,0,0,0,0},
+                     {0,0,0,0,0,0,0,0},
+                     {0,0,0,0,0,0,0,0},
+                     {0,0,0,1,1,1,0,0},
+                     {0,0,0,2,1,0,0,0},
+                     {0,0,0,0,0,0,0,0},
+                     {0,0,0,0,0,0,0,0},
+                     {0,0,0,0,0,0,0,0}
+                };
+    scanf("%d%d", &posX, &posY);
+    sltDisk=reversi[posX][posY];
+    printf("%d",find_chess(reversi[BOARD_SIZE][BOARD_SIZE],posX,posY,sltDisk));
+    
+    printf("\n白子可下位置:\n");
+    for(int i=0;i<BOARD_SIZE;i++)
+        for(int j=0;j<BOARD_SIZE;j++){
+            if(reversi[i][j] == 1){
+                print_steps(reversi,i,j,reversi[i][j]);
+            }
     }
-        return 0;
+   printf("\n黑子可下位置:\n");
+   for(int i=0;i<BOARD_SIZE;i++)
+        for(int j=0;j<BOARD_SIZE;j++){
+            if(reversi[i][j] == 2){
+                print_steps(reversi,i,j,reversi[i][j]);
+            }
+        }
 }
